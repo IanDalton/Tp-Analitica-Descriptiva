d5a0240db1bab8ccb33763dd42443e231bf5391e
IanDalton <daltioan@gmail.com>
2023-05-25 01:05:14 -0300

    Group analysis + heatmap

--- analisis.r
+++ analisis.r
@@ -2,8 +2,9 @@
 library(readxl)
 library(dplyr)
 library(ggplot2)
-library(tydiverse)
+library(tidyverse)
 library(extrafont)
+library(stringr)
 loadfonts(device = "win")
 setwd("S:/Github/Tp-Analitica-Descriptiva")
 data <- read_excel("dataset terrorismo.xlsx")
@@ -94,111 +95,4 @@ data_us %>%
     summarise(n = n()) %>%
     chow.test(n ~ iyear, data = .)
 
-# Creating a heatmap of where the attacks happened and the amount of attacks in each country
 
-world_coordinate <- map_data("world")
-
-# Creating data_total to get the amount of attacks in each country
-# But before ploting I remove the outliers in n
-
-data_total <- data %>%
-    group_by(country_txt, longitude, latitude) %>%
-    filter(!is.na(longitude) & !is.na(latitude)) %>%
-    count()
-data_in_america <- data %>%
-    filter(region %in% c(1, 2, 3)) %>%
-    group_by(country_txt, longitude, latitude) %>%
-    filter(!is.na(longitude) & !is.na(latitude)) %>%
-    count()
-
-ggplot() +
-    geom_map(
-        data = world_coordinate, map = world_coordinate,
-        aes(long, lat, map_id = region),
-        color = "black", fill = "lightyellow"
-    ) +
-    stat_summary_2d(
-        data = data_total,
-        aes(longitude, latitude, z = n),
-        alpha = 1,
-        fun = sum,
-        binwidth = c(1, 1)
-    ) +
-    scale_fill_gradient(low = "green", high = "red")
-
-# creating a heatmap of the amount of attacks in American continent
-america_map <- map_data("world", region = c("Canada", "United States", "Mexico", "Central America", "South America"))
-
-ggplot() +
-    geom_map(
-        data = america_map, map = america_map,
-        aes(long, lat, map_id = region),
-        color = "black", fill = "lightyellow"
-    ) +
-    stat_summary_hex(
-        data = data_in_america,
-        aes(longitude, latitude, z = n),
-        alpha = 1,
-        fun = sum,
-        binwidth = c(2, 2),
-        geom = "hex"
-    )
-
-mapdata <- map_data("world")
-
-
-mapdata <- left_join(mapdata,
-        data %>%
-            group_by(country_txt) %>%
-            count() %>%
-            mutate( country_txt = if( country_txt == "United States"){"USA"}else{country_txt}),
-        by = c("region" = "country_txt")
-    )
-mapdata1 <- mapdata %>%
-    filter(!is.na(n))
-
-ggplot(mapdata1, aes(long,lat,group = group)) +
-    geom_map(
-        data = world_coordinate, map = world_coordinate,
-        aes(long, lat, map_id = region),
-        color = "black", fill = "white"
-    )+
-    geom_polygon(aes(fill = log(n)), color = "black")+
-    scale_fill_gradient(low = "green", high = "red")+
-    labs(
-        x = "Longitud",
-        y = "Latitud",
-        title = "Cantidad de ataques por país",
-        fill = "Cantidad de ataques",
-        caption = "NOTA: Se muestra el logaritmo de la cantidad de ataques por país"
-    ) +
-    theme(text = element_text(family = "Comic Sans MS"))
-    
-#creating a heatmap with the atackers
-
-mapdata <- map_data("world")
-
-mapdata <- left_join(mapdata,
-        data %>%
-            group_by(natlty1_txt) %>%
-            filter(!is.na(natlty1_txt)) %>%
-            count() %>%
-            mutate( natlty1_txt = if( natlty1_txt == "United States"){"USA"}else{natlty1_txt}) %>%
-            filter(!is.na(n)),
-        by = c("region" = "natlty1_txt")
-    )
-ggplot(mapdata, aes(long,lat,group = group)) +
-    geom_map(
-        data = world_coordinate, map = world_coordinate,
-        aes(long, lat, map_id = region),
-        color = "black", fill = "white"
-    )+
-    geom_polygon(aes(fill = log(n)), color = "black")+
-    scale_fill_gradient(low = "green", high = "red")+
-    labs(
-        x = "Longitud",
-        y = "Latitud",
-        title = "Cantidad de victimas por país",
-        fill = "Cantidad de victimas",
-        caption = "NOTA: Se muestra el logaritmo de la cantidad de victimas por país"
-    )
---
+++ group_analysis.r
@@ -0,0 +1,131 @@
+# Reading Excel file
+library(readxl)
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+library(extrafont)
+library(stringr)
+loadfonts(device = "win")
+setwd("S:/Github/Tp-Analitica-Descriptiva")
+data <- read_excel("dataset terrorismo.xlsx")
+
+# getting the top 5 groups that have claimed the most attacks and showing them as a table
+
+data %>%
+    filter(gname != "Unknown") %>%
+    group_by(gname) %>%
+    summarise(nAttacks = n()) %>%
+    arrange(desc(nAttacks)) %>%
+    head(5) %>%
+    ggplot(aes(x = reorder(gname, -nAttacks), y = nAttacks)) +
+        geom_bar(stat = "identity") +
+        labs(
+            x = "Grupo",
+            y = "Cantidad de ataques",
+            title = "Top 5 grupos con más ataques",
+            caption = "NOTA: Se descartaron los casos en los que el grupo no fue identificado")
+
+# getting the top 5 groups that have claimed the most victims and showing them as a table
+
+data %>%
+    group_by(gname) %>%
+    summarise(nKills = sum(nkill)) %>%
+    arrange(desc(nKills)) %>%
+    head(5) %>%
+    ggplot(aes(x = reorder(gname, -nKills), y = nKills)) +
+        geom_bar(stat = "identity") +
+        labs(
+            x = "Grupo",
+            y = "Cantidad de fallecidos",
+            title = "Top 5 grupos con más fallecidos"
+        )
+
+data %>%
+    group_by(gname) %>%
+    summarise(nWound= sum(nwound)) %>%
+    arrange(desc(nWound)) %>%
+    head(5) %>%
+    mutate(gname = str_wrap(gname, width = 10)) %>%
+    ggplot(aes(x = reorder(gname, -nWound), y = nWound)) +
+        geom_bar(stat = "identity") +
+        labs(
+            x = "Grupo",
+            y = "Cantidad de heridos",
+            title = "Top 5 grupos con más heridos"
+        )
+
+#Showing the evolution over time of the top 5 groups with the most attacks
+
+top_5 = data %>%
+    filter(gname != "Unknown") %>%
+    group_by(gname) %>%
+    summarise(nAttacks = n()) %>%
+    arrange(desc(nAttacks)) %>%
+    head(5)
+data %>%
+    filter(gname %in% top_5$gname) %>%
+    group_by(iyear, gname) %>%
+    summarise(nAttacks = n()) %>%
+    arrange(desc(nAttacks)) %>%
+    ggplot(aes(x = iyear, y = nAttacks, color = gname)) +
+        geom_line() +
+        labs(
+        x = "Año",
+        y = "Cantidad de ataques",
+        title = "Evolución de los ataques de los 5 grupos con más ataques",
+        )
+# Piechart showing the atack types of Al-Qaida and Aum Shinri kyo
+
+data %>%
+    filter(gname == "Al-Qaida") %>%
+    group_by(attacktype1_txt) %>%
+    summarise(nAttacks = n()) %>%
+    ggplot(aes(x = "", y = nAttacks, fill = attacktype1_txt)) +
+        geom_bar(stat = "identity") +
+        coord_polar("y", start = 0) +
+        labs(
+            x = "",
+            y = "",
+            title = "Tipos de ataques de Al-Qaida",
+            fill = "Tipo de ataque"
+        )
+# Showing the same data but over time using bars
+data %>%
+    filter(gname == "Al-Qaida") %>%
+    group_by(iyear, attacktype1_txt) %>%
+    summarise(nAttacks = n()) %>%
+    ggplot(aes(x = iyear, y = nAttacks, fill = attacktype1_txt)) +
+        geom_bar(stat = "identity") +
+        labs(
+            x = "Año",
+            y = "Cantidad de ataques",
+            title = "Evolución de los tipos de ataques de Al-Qaida",
+            fill = "Tipo de ataque"
+        )
+# Now the same thing but for Aum Shinri kyo
+data %>%
+    filter(gname == "Aum Shinri Kyo") %>%
+    group_by(attacktype1_txt) %>%
+    summarise(nAttacks = n()) %>%
+    ggplot(aes(x = "", y = nAttacks, fill = attacktype1_txt)) +
+        geom_bar(stat = "identity") +
+        coord_polar("y", start = 0) +
+        labs(
+            x = "",
+            y = "",
+            title = "Tipos de ataques de Aum Shinri Kyo",
+            fill = "Tipo de ataque"
+        )
+# Showing the same data but over time using bars
+data %>%
+    filter(gname == "Aum Shinri Kyo") %>%
+    group_by(iyear, attacktype1_txt) %>%
+    summarise(nAttacks = n()) %>%
+    ggplot(aes(x = iyear, y = nAttacks, fill = attacktype1_txt)) +
+        geom_bar(stat = "identity") +
+        labs(
+            x = "Año",
+            y = "Cantidad de ataques",
+            title = "Evolución de los tipos de ataques de Aum Shinri Kyo",
+            fill = "Tipo de ataque"
+        )
